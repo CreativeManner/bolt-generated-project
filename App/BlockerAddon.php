7c33a6f3fafeacb34e4b533e9499587704a0f05a
Oz Coruhlu <oz@creativemanner.com>
2024-12-12 23:11:12 -0500

    Initial commit from your app

--- App/BlockerAddon.php
+++ App/BlockerAddon.php
@@ -13,18 +13,21 @@ class BlockerAddon extends AddonLoader
 
     public function init()
     {
-        // Add direct action hooks instead of relying on initHooks
-        add_action('bkntc_enqueue_assets', [$this, 'enqueueAssets']);
-        add_action('bkntc_calendar_page_footer', [$this, 'injectModalTemplate']);
-        
-        // Critical: Hook into the calendar toolbar
+        // Hook into Booknetic's calendar
+        add_action('bkntc_calendar_footer', [$this, 'injectModalTemplate']);
         add_action('bkntc_calendar_toolbar_after', [$this, 'injectBlockerButton']);
         
+        // Add assets
+        add_action('bkntc_enqueue_assets', [$this, 'enqueueAssets']);
+
         // Ajax handlers
         add_action('wp_ajax_booknetic_save_blocker_settings', [$this, 'ajaxSaveBlockerSettings']);
         add_action('wp_ajax_booknetic_add_block', [$this, 'ajaxAddBlock']);
         add_action('wp_ajax_booknetic_get_blocks', [$this, 'ajaxGetBlocks']);
         add_action('wp_ajax_booknetic_delete_block', [$this, 'ajaxDeleteBlock']);
+
+        // Filter calendar events to show blocks
+        add_filter('bkntc_calendar_events', [$this, 'addBlockedEvents'], 10, 2);
     }
 
     public function getAddonInfo()
@@ -41,12 +44,11 @@ class BlockerAddon extends AddonLoader
 
     public function enqueueAssets()
     {
-        // Only load assets on calendar page
+        // Only load on calendar page
         if (!Helper::isCalendarPage()) {
             return;
         }
 
-        // Enqueue CSS
         wp_enqueue_style(
             'booknetic-blocker', 
             plugins_url('assets/css/blocker.css', dirname(__FILE__)), 
@@ -54,7 +56,6 @@ class BlockerAddon extends AddonLoader
             self::$version
         );
 
-        // Enqueue JavaScript
         wp_enqueue_script(
             'booknetic-blocker',
             plugins_url('assets/js/blocker.js', dirname(__FILE__)),
@@ -63,7 +64,6 @@ class BlockerAddon extends AddonLoader
             true
         );
 
-        // Localize script
         wp_localize_script('booknetic-blocker', 'BookneticBlockerData', [
             'enabled' => Helper::getOption('booknetic_blocker_enabled', '1'),
             'ajaxUrl' => admin_url('admin-ajax.php'),
@@ -83,10 +83,44 @@ class BlockerAddon extends AddonLoader
         }
 
         ?>
-        <button type="button" class="btn btn-primary" id="booknetic-blocker-btn">
-            <i class="fa fa-ban"></i> <?php echo __('Block Time', 'booknetic-blocker'); ?>
-        </button>
+        <div class="calendar-toolbar-container">
+            <button type="button" class="btn btn-primary" id="booknetic-blocker-btn">
+                <i class="fa fa-ban"></i> <?php echo __('Block Time', 'booknetic-blocker'); ?>
+            </button>
+        </div>
         <?php
+    }
+
+    public function injectModalTemplate()
+    {
+        require_once dirname(__DIR__) . '/view/blocker_interface.php';
+    }
+
+    public function addBlockedEvents($events, $parameters)
+    {
+        if (!Helper::getOption('booknetic_blocker_enabled', '1')) {
+            return $events;
+        }
+
+        $blocks = Models\BlockedSlot::getByStaffAndDateRange(
+            $parameters['staff_id'],
+            $parameters['start'],
+            $parameters['end']
+        );
+
+        foreach ($blocks as $block) {
+            $events[] = [
+                'id' => 'block_' . $block->id,
+                'title' => __('Blocked', 'booknetic-blocker'),
+                'start' => $block->date . ' ' . $block->start_time,
+                'end' => $block->date . ' ' . $block->end_time,
+                'className' => 'booknetic-blocked-slot',
+                'color' => '#ff4d4d',
+                'is_blocked' => true
+            ];
+        }
+
+        return $events;
     }
 
     // ... rest of the methods remain the same ...
